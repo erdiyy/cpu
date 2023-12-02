fdaa9028af94dba52a8fc04e2bc8bbbb3c491fd3
erdiyy <152510363+erdiyy@users.noreply.github.com>
2023-12-02 05:03:02 +0300

    Create cpu_hack.sh
    @echo off
    cd %~dp0
    cls
    
    SRBMiner-MULTI.exe --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHsBrr5d5c3KLttBxKrt1HWzUsEcPwvVvYe9SdmV2Shey9S5SbEUaSH2uSaguskrihrFwpNg5ucNbu4KaEQzw51wpzeRmuXSe --password YOUR_WORKER_NAME
    pause

---
+++ cpu_hack.sh
@@ -0,0 +1,6 @@
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHsBrr5d5c3KLttBxKrt1HWzUsEcPwvVvYe9SdmV2Shey9S5SbEUaSH2uSaguskrihrFwpNg5ucNbu4KaEQzw51wpzeRmuXSe --password YOUR_WORKER_NAME
+pause

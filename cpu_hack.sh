c0ae27fac188fbcf19035bd98b0ad6a34f772a6c
erdiyy <152510363+erdiyy@users.noreply.github.com>
2023-12-03 01:58:27 +0300

    Update cpu_hack.sh88

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
 tar xf xmrig-6.21.0-linux-64.tar.gz
 cd xmrig-6.21.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u xmr:82UNyr6tGBH5YHc8JwvqKiUzaHtfpx2wMijaZThfG3RuMdJh15ueqKoQqCisrrkGDjLURFLcy3kNXEYDN7RjWpp16sQ5JiZ.Erdiyy#ek61-6h9x -p x --cpu 2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TLhZonou51kKYSirrgoL7PBzCNAw7Eg7eT.Erdiyy#ek61-6h9x -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
